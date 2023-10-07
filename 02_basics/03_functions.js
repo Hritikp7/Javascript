9df44d8b4a9116e0642d3d89938db35802f50282
Hritikp7 <111853491+Hritikp7@users.noreply.github.com>
2023-10-07 12:28:52 +0000

    fuctions and scopes

---
+++ 02_basics/03_functions.js
@@ -0,0 +1,21 @@
+// function calculateCartPrice(...num1) {
+//     let sum = 0
+//     for (let i = 0; i < num1.length; i++) {
+//         sum = sum + num1[i]
+//     }
+//  return sum
+// }
+
+// const price = calculateCartPrice(100,200,300)
+// console.log(price);
+
+// const user = {
+//     username: "Hritik",
+//     price: 99
+// }
+
+// function handleObject(anyObject) {
+//     console.log(`Username is ${anyObject.username} and price is ${anyObject.price}`);
+// }
+
+// handleObject(user);
---
+++ 02_basics/04_scopes.js
@@ -0,0 +1,11 @@
+let num1 = 200
+const num2 = 400
+if (true) {
+  let num1 = 10;
+  const num2 = 20;
+  var num3 = 30;
+}
+
+console.log(num1);
+console.log(num2);
+console.log(num3);
