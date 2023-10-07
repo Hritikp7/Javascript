8fb27a17dea8f8a7559e436c5c8fb792691228ce
Hritikp7 <111853491+Hritikp7@users.noreply.github.com>
2023-10-07 09:51:45 +0000

    strings and math complete

---
+++ 01_basics/04_string.js
@@ -0,0 +1,12 @@
+const name = "Hritik"
+const repoCount = 50
+
+console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
+
+const str = new String("Pawar");
+console.log(str);
+console.log(typeof str);
+
+console.log(str.length);
+console.log(str.toUpperCase());
+console.log(str);
---
+++ 01_basics/05_numbers_and_math.js
@@ -0,0 +1,9 @@
+const num1 = 400
+
+console.log(num1);
+
+const num2 = new Number(100)
+console.log(num2);
+
+const otherNum = 123.49;
+console.log(otherNum.toPrecision(3));
