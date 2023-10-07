d98d6dfd4599a413d9f1c1187fe5f6b5cc49a2d7
Hritikp7 <111853491+Hritikp7@users.noreply.github.com>
2023-10-07 08:07:06 +0000

    datatypes commit

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,13 @@
+let name = "Hritik"
+let age = 23
+let islogged = true
+let s = undefined
+let n = null
+let bi = 23736178251835125253612351267n;
+
+console.log(typeof name);
+console.log(typeof age);
+console.log(typeof islogged);
+console.log(typeof s);
+console.log(typeof n);
+console.log(typeof bi);
