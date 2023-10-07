3eaa2edf137daf8759c7b4bd8093c25a4d0b2748
Hritikp7 <111853491+Hritikp7@users.noreply.github.com>
2023-10-07 08:24:53 +0000

    more coversion

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,10 +1,15 @@
-let score = 33;
-console.log(typeof(score));
+// let score = 33;
+// console.log(typeof(score));
 
-let value = Number(score);
-console.log(typeof(NaN));
+// let value = Number(score);
+// console.log(typeof(NaN));
 
 
-let IsLoggedIn = 1;
-let booleanIsLoggedAIn = Boolean(IsLoggedIn);
-console.log(booleanIsLoggedAIn);
+// let IsLoggedIn = 1;
+// let booleanIsLoggedAIn = Boolean(IsLoggedIn);
+// console.log(booleanIsLoggedAIn);
+
+console.log("1" + 2);
+console.log(1 + "2");
+console.log("1" + 2 + 3);
+console.log(1 + 2 + "3");
