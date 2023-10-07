250518b8cfd3fdda15b10bae8fb0eebb558f8106
Hritikp7 <111853491+Hritikp7@users.noreply.github.com>
2023-10-07 07:29:41 +0000

    variables commit

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,16 @@
+const accountId = 123232;
+let accountEmail = "hritikpawar@gmail.com";
+var accountPassword = "1234";
+accountCity = "Pune";
+
+// accountId = 2;
+accountEamil = "hritk@gmailcom";
+accountPassword = 453345;
+{
+    var accountPassword = 34534;
+    console.log(accountId);
+    console.log(accountPassword);
+}
+console.log(accountPassword);
+
+console.table([accountId,accountEmail,accountPassword,accountCity])
