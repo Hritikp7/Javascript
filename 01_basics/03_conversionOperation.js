63e9a348629d414d1476e688fee9e32f9b4f5423
Hritikp7 <111853491+Hritikp7@users.noreply.github.com>
2023-10-07 08:17:06 +0000

    conversion of data types

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,10 @@
+let score = 33;
+console.log(typeof(score));
+
+let value = Number(score);
+console.log(typeof(NaN));
+
+
+let IsLoggedIn = 1;
+let booleanIsLoggedAIn = Boolean(IsLoggedIn);
+console.log(booleanIsLoggedAIn);
